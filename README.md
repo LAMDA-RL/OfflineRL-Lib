0cd18b0797d9ae4498cd80b37dbfae01fba18abf
typoverflow <typoverflow@outlook.com>
2023-02-13 11:36:34 +0800

    update: ready for making public

---
+++ README.md
@@ -0,0 +1,7 @@
+# OfflineRL-Lib
+> 🚧 This repo is not ready for release, benchmarking is ongoing. 🚧
+
+OfflineRL-Lib provides unofficial and benchmarked PyTorch implementations for selected OfflineRL algorithms, including: 
+- [Extreme Q-Learning](https://arxiv.org/abs/2301.02328)
+
+Currently OfflineRL-Lib is heavily based off [UtilsRL](https://github.com/typoverflow/UtilsRL), and we will release a standalone version once it is ready. 
---
+++ setup.py
@@ -0,0 +1,40 @@
+
+
+import pathlib
+from setuptools import setup, find_packages
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+
+ROOT_DIR = pathlib.Path(__file__).parent
+README = (ROOT_DIR / "README.md").read_text()
+VERSION = (ROOT_DIR / "VERSION").read_text()
+
+def get_install_requires():
+    return [
+        "gym>=0.23.1,<=0.24.1", 
+        "tqdm", 
+        "numpy", 
+        "torch", 
+        "pandas", 
+        "UtilsRL"
+    ]
+
+def get_extras_require():
+    return {}
+
+setup(
+    name                = "offlinerllib", 
+    version             = VERSION, 
+    description         = "A python module desgined for Offline RL algorithms developing and benchmarking. ", 
+    long_description    = README,
+    long_description_content_type = "text/markdown",
+    url                 = "https://github.com/typoverflow/OfflineRLLib",
+    author              = "typoverflow", 
+    author_email        = "typoverflow@outlook.com", 
+    license             = "MIT", 
+    packages            = find_packages(),
+    include_package_data = True, 
+    tests_require=["pytest", "mock"], 
+    python_requires=">=3.7", 
+    install_requires = get_install_requires(), 
+    # extras_require = get_extras_require(), 
+)
