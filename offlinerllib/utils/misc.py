2b78b49698dd2efb5319a7ac1230730d83075d36
typoverflow <typoverflow@outlook.com>
2023-02-25 01:36:07 +0800

    update: remove merge dict

--- offlinerllib/utils/misc.py
+++ offlinerllib/utils/misc.py
@@ -13,10 +13,3 @@ def make_target(m: nn.Module) -> nn.Module:
 def get_attributes(obj) -> Dict[str, Any]:
     return dict(inspect.getmembers(obj))
 
-def merge_dict(dicts: List[Dict[str, torch.Tensor] | Dict[str, float]], is_tensor: bool=False) -> Dict[str, float]:
-    result = {}
-    for dict in dicts:
-        result.update(dict)
-    if is_tensor:
-        result = {key:value.item() for (key, value) in result.items()}
-    return result
