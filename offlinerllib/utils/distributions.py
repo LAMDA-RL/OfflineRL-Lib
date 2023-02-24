a77f9dc3e3721a4e175752224f60000dd409d490
typoverflow <typoverflow@outlook.com>
2023-02-24 10:20:32 +0800

    update: cleaning up

--- offlinerllib/utils/distributions.py
+++ offlinerllib/utils/distributions.py
@@ -13,7 +13,7 @@ class TanhNormal(Normal):
                  ):
         super().__init__(loc, scale)
         self.epsilon = np.finfo(np.float32).eps.item()
-    
+        
     def log_prob(self, 
                  value: torch.Tensor, 
                  pre_tanh_value: bool=False, 
@@ -25,8 +25,7 @@ class TanhNormal(Normal):
             pre_value = value
             value = torch.tanh(pre_value)
         return super().log_prob(pre_value) - 2*(math.log(2.0) - pre_value - torch.nn.functional.softplus(-2 * pre_value))
-        return super().log_prob(pre_value) - torch.log((1-value.pow(2)) + self.epsilon)
-       
+    
     def sample(self, sample_shape: Union[Sequence[int], int]=torch.Size([]), return_raw: bool=False):
         z = super().sample(sample_shape)
         return (torch.tanh(z), z) if return_raw else torch.tanh(z)
