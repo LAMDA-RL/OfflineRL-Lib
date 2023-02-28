2eeb5a5ad35b83235bc585b87510ebdff195894a
typoverflow <typoverflow@outlook.com>
2023-02-28 21:43:04 +0800

    feat: Advantage Weighted Actor Critic (#5)

---
+++ reproduce/awac/config/offline/original/mujoco/hopper-random-v2.py
@@ -0,0 +1,4 @@
+from reproduce.awac.config.offline.original.base import *
+
+task = "hopper-random-v2"
+aw_lambda = 1.0
---
+++ reproduce/awac/config/offline/original/mujoco/walker2d-full-replay-v2.py
@@ -0,0 +1,4 @@
+from reproduce.awac.config.offline.original.base import *
+
+task = "walker2d-full-replay-v2"
+aw_lambda = 1.0
