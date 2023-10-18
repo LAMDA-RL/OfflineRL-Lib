fc6cbb3705f1d1536924fd1e5990926f62d7601d
typoverflow <typoverflow@outlook.com>
2023-10-18 23:01:50 +0800

    fix: EnsembleLinear is not properly initialized

--- offlinerllib/module/net/basic.py
+++ offlinerllib/module/net/basic.py
@@ -88,7 +88,7 @@ class EnsembleLinear(nn.Module):
             torch.nn.init.kaiming_uniform_(self.weight[..., i], a=math.sqrt(5))
         if self.add_bias:
             for i in range(self.ensemble_size):
-                fan_in, _ = torch.nn.init._calculate_fan_in_and_fan_out(self.weight[i])
+                fan_in, _ = torch.nn.init._calculate_fan_in_and_fan_out(self.weight[..., i])
                 bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
                 torch.nn.init.uniform_(self.bias[..., i], -bound, bound)
         
