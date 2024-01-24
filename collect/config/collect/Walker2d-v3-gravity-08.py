eae14cffa771793a9a23d42a5527c71586f01504
typoverflow <typoverflow@outlook.com>
2024-01-25 00:56:13 +0800

    dev: update collect

---
+++ collect/config/collect/Walker2d-v3-gravity-08.py
@@ -0,0 +1,5 @@
+from collect.config.collect.base import *
+
+task = "Walker2d-v3"
+overwrite_args = {"gravity": 0.8}
+do_scale = True
---
+++ collect/config/collect/Walker2d-v3-gravity-12.py
@@ -0,0 +1,5 @@
+from collect.config.collect.base import *
+
+task = "Walker2d-v3"
+overwrite_args = {"gravity": 1.2}
+do_scale = True
